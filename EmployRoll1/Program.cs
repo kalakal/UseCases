d6a112bb8231529fa07c0feb2617d822f26237ab
kalakal <sayedfaizanqadri@gmail.com>
2022-10-31 17:19:02 +0530

    UseCase7 Added

--- EmployRoll1/Program.cs
+++ EmployRoll1/Program.cs
@@ -3,45 +3,50 @@
 namespace EmployRoll1
 
 {
-    //UseCase6
+    //UseCase7
 
-    internal class Program
-    {
-        public const int IS_PART_TIME = 1;
-        public const int IS_FULL_TIME = 2;
-        public const int EMP_RATE_PER_HOUR = 20;
-        public const int NUM_OF_WORKING_DAYS = 2;
-        public const int MAX_HOURS_IN_MONTH = 10;
-
-        static void Main(string[] args)
+        internal class Program
         {
-            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
-            while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day8 :" + totalWorkingDays + "Emp Hrs :" + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
-            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
-        }
+            public const int IS_PART_TIME = 1;
+            public const int IS_FULL_TIME = 2;
+            public const int EMP_RATE_PER_HOUR = 20;
+            public const int NUM_OF_WORKING_DAYS = 20;
+            public const int MAX_HOURS_IN_MONTH = 15;
 
+            public static int computeEmpWage()
+            {
+                int empHrs = 0, totalempHrs = 0, totalWorkingDays = 0;
+                while (totalempHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+                {
+                    totalWorkingDays++;
+                    Random random = new Random();
+                    int empCheck = random.Next(empHrs);
+                    switch (empCheck)
+                    {
+                        case IS_PART_TIME:
+                            empHrs = 4;
+                            break;
+                        case IS_FULL_TIME:
+                            empHrs = 8;
+                            break;
+                        default:
+                            empHrs = 0;
+                            break;
+                    }
+                    totalempHrs += empHrs;
+                    Console.WriteLine("Day#:" + totalWorkingDays + "Emp Hrs :" + empHrs);
+                }
+                int totalEmpWage = totalempHrs * EMP_RATE_PER_HOUR;
+                Console.WriteLine("Total Emp wage :" + totalEmpWage);
+                return totalEmpWage;
+            }
+            static void Main(string[] args)
+            {
+                computeEmpWage();
+            }
+
+        }
     }
-}
    
 
 
