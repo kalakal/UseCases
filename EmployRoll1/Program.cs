0b6b0cf6fdb9766901dbc12a84f1fea25129051a
kalakal <sayedfaizanqadri@gmail.com>
2022-10-31 17:02:14 +0530

    UseCase4ADDED

--- EmployRoll1/Program.cs
+++ EmployRoll1/Program.cs
@@ -3,34 +3,37 @@
 namespace EmployRoll1
 
 {
-    //UseCase3
+    //UseCase4
+
     internal class Program
     {
+        public const int IS_PART_TIME = 1;
+        public const int IS_FULL_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 20;
         static void Main(string[] args)
         {
-            int isPartTime = 1;
-            int isFullTime = 2;
-            int empRatePerHr = 20;
-            int emphr = 0;
+            int empHrs = 0;
             int empWage = 0;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            if (empCheck == isPartTime)
+            switch (empCheck)
             {
-                emphr = 4;
-            }
-            else if (empCheck == isFullTime)
-            {
-                emphr = 8;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
+
 
             }
-            else
-            {
-                emphr = 0;
-            }
-            empWage = emphr * empRatePerHr;
-            Console.WriteLine("EmpWage = :" + empWage);
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Emp Wage :" + empWage);
         }
+
     }
 }
    
