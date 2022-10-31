11515d2ef44c5b494c4ef57abd66cb799f237f71
kalakal <sayedfaizanqadri@gmail.com>
2022-10-31 17:14:19 +0530

    UseCase added

--- EmployRoll1/Program.cs
+++ EmployRoll1/Program.cs
@@ -3,7 +3,7 @@
 namespace EmployRoll1
 
 {
-    //UseCase5
+    //UseCase6
 
     internal class Program
     {
@@ -11,11 +11,14 @@ namespace EmployRoll1
         public const int IS_FULL_TIME = 2;
         public const int EMP_RATE_PER_HOUR = 20;
         public const int NUM_OF_WORKING_DAYS = 2;
+        public const int MAX_HOURS_IN_MONTH = 10;
+
         static void Main(string[] args)
         {
-            int empHrs = 0, empWage = 0, totalEmpWage = 0;
-            for (int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+            int empHrs = 0, totalEmpHrs = 0, totalWorkingDays = 0;
+            while (totalEmpHrs <= MAX_HOURS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
             {
+                totalWorkingDays++;
                 Random random = new Random();
                 int empCheck = random.Next(0, 3);
                 switch (empCheck)
@@ -30,12 +33,11 @@ namespace EmployRoll1
                         empHrs = 0;
                         break;
                 }
-                empWage = empHrs * EMP_RATE_PER_HOUR;
-                empWage += empWage;
-                Console.WriteLine("Emp Wage : " + empWage);
-
+                totalEmpHrs += empHrs;
+                Console.WriteLine("Day8 :" + totalWorkingDays + "Emp Hrs :" + empHrs);
             }
-            Console.WriteLine("Total Emp Wage : " + totalEmpWage);
+            int totalEmpWage = totalEmpHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Total Emp Wage :" + totalEmpWage);
         }
 
     }
