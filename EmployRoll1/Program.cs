7a405b9378457862753393b82fbeb8094c6465cb
kalakal <sayedfaizanqadri@gmail.com>
2022-10-31 16:39:20 +0530

    added usecases

---
+++ EmployRoll1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace EmployRoll1
+
+{
+    //usecase1
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int value = random.Next(0, 1);
+            if (value == 1)
+            {
+                Console.WriteLine("present");
+            }
+            else
+                Console.WriteLine("not present");
+        }
+    }
+}
+
